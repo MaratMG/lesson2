e0143e46026bb6ac086d19bcc58affc506768eef
MaratMG <maratmg85@gmail.com>
2024-09-12 21:44:12 +0500

    Add files via upload

---
+++ Task3..py
@@ -0,0 +1,5 @@
+number_of_tasks =12
+number_of_hours_spent =1.5
+course_name ='Python'
+Time_per_task =(number_of_hours_spent / number_of_tasks)
+print ('Курс:',course_name,', всего задач:',number_of_tasks,', затрачено часов:',number_of_hours_spent,', среднее время выполнения', Time_per_task, 'часа')
